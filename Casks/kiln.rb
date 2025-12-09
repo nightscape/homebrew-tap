db7c4d33ef33a8864f033eceb95fd08dc5fbbba3
Martin Mauch <martin@mauch.dev>
2025-12-09 08:48:14 +0100

    kiln 0.23.0

--- Casks/kiln.rb
+++ Casks/kiln.rb
@@ -1,8 +1,9 @@
 cask "kiln" do
-  version "0.10.0"
-  sha256 "b94c5e75946d73ad5d9ea076509f71b0ddc04fbef34c780f793c375fa946adba"
+  version "0.23.0"
+  sha256 "3cfae347f890a14c676dc97b97d6a099c31b0243ae9156ba8f030b32898a3d43"
 
-  url "https://github.com/Kiln-AI/Kiln/releases/download/v#{version}/Kiln.MacOS.AppleSilicon.M-Processor.dmg", verified: "github.com/Kiln-AI/Kiln/"
+  url "https://github.com/Kiln-AI/Kiln/releases/download/v#{version}/Kiln.MacOS.AppleSilicon.M-Processor.dmg",
+      verified: "github.com/Kiln-AI/Kiln/"
   name "kiln"
   desc "Fine-tune LLM models, generate synthetic data, and collaborate on datasets"
   homepage "https://docs.getkiln.ai/"
