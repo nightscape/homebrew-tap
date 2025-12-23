5f8c176226dfe7d21f32924be56edd9cd61f3a87
Martin Mauch <martin.mauch@gmail.com>
2025-12-23 00:05:20 +0000

    kinetic-merge 1.13.1

--- Formula/kinetic-merge.rb
+++ Formula/kinetic-merge.rb
@@ -1,9 +1,9 @@
 class KineticMerge < Formula
   desc "Merge a heavily refactored codebase and stay sane"
   homepage "https://github.com/sageserpent-open/kineticMerge"
-  url "https://github.com/sageserpent-open/kineticMerge/releases/download/v1.13.0/kinetic-merge"
+  url "https://github.com/sageserpent-open/kineticMerge/releases/download/v1.13.1/kinetic-merge"
 
-  sha256 "a84dfc431a1c301f545c8d86df580546fceff46b78d0d7012a5c4b9f70e1fe88"
+  sha256 "a66b1d5b5ce644ccfe45bf7c0f3ec8b8d882d4190fa392e0edf6c3e331682641"
 
   license "MIT"
 
