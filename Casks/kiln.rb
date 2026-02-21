669da12062c8de32d563cd82cff42e26adf5f97d
Martin Mauch <martin@mauch.dev>
2026-02-21 00:07:31 +0000

    kiln 0.25.0

--- Casks/kiln.rb
+++ Casks/kiln.rb
@@ -1,6 +1,6 @@
 cask "kiln" do
-  version "0.24.0"
-  sha256 "805b29cb34a4009e5e64f7719d4343771b9a9557a5c9eed7f83f9a017d5c8ef6"
+  version "0.25.0"
+  sha256 "b86c827453f66c7f40b3d9f023c55ca78a6b8dc21beef02979617b7c9442d700"
 
   url "https://github.com/Kiln-AI/Kiln/releases/download/v#{version}/Kiln.MacOS.AppleSilicon.M-Processor.dmg",
       verified: "github.com/Kiln-AI/Kiln/"
