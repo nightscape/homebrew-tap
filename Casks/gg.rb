4f2e758bb7fa27ed4f7804e750fb966a158e68fe
Martin Mauch <martin@mauch.dev>
2026-03-13 00:07:54 +0000

    gg 0.39.1

--- Casks/gg.rb
+++ Casks/gg.rb
@@ -1,6 +1,6 @@
 cask "gg" do
-  version "0.38.2"
-  sha256 "29e2c7d08ebee59bede13c22774d6059ba82c7b7148907acc0dfa379ad68f5b3"
+  version "0.39.1"
+  sha256 "98b88d598cf614cbf722df9abbd9e2a2b218cf5e8b6b8ea9b94890cc666c879f"
 
   url "https://github.com/gulbanana/gg/releases/download/v#{version}/gg_#{version}_universal.dmg"
   name "gg"
